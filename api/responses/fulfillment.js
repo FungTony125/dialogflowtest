0a81840e8cc0ba22418a1ed72987b3515c7218b3
Tony Fung <18200443@life.hkbu.edu.hk>
2019-03-20 17:30:12 +0800

    try to get myoutputcontexts3

--- api/responses/fulfillment.js
+++ api/responses/fulfillment.js
@@ -25,7 +25,7 @@ module.exports = function () {
     console.log(conv);
     let params = agent.parameters;
      surgery = params.surgery;
-    console.log("The surgery 12541 is "+surgery);
+    console.log("The surgery 9999999 is "+surgery);
     return agent.add('請輸入負責手術的醫生名字，如不知道請輸入"0"**(in sailed)');
   }
 
@@ -34,11 +34,7 @@ module.exports = function () {
     console.log(":::::::::::in user provide doctor name::::::::::::");
     console.log(conv);
 
-    var myoutputcontexts = agent.context.get('myoutputcontexts');
-      console.log('myoutputcontexts: '+myoutputcontexts);
-      var contextSurgery = myoutputcontexts.parameters.surgery;
-      
-      console.log(contextSurgery);
+    
 
     return agent.add('胃鏡基線案例收費通常為8300至13300，基線案例：A.沒息肉切除 B.一般胃鏡 C.沒有併發症/低風險/另加手術/急症 D. 醫院日間手術室 E.無麻醉醫生 F.住0/1晚。主要影響收費的選項及某些個案價的附加費如下：\n<=3有息肉切除<=1cm：4,700.\n<=3有息肉切除<=1cm：1,000.\n>3息肉切除,<=1cm：6,800.\n自體熒光成像(癌前損傷)：3,900.\n超細胃鏡5mm(小孩/老人)：1,700.\n膠囊內鏡：11,800.\n有併發症/高至中風險/另加手術/急症	的改費我們暫時未有案例。\n\n可以?(in sailed)');
     
@@ -48,7 +44,13 @@ module.exports = function () {
     let conv = agent.conv();
     console.log(":::::::::::in user case confirm::::::::::::");
     console.log(conv);
-  
+    
+    var myoutputcontexts = agent.context.get('myoutputcontexts');
+    console.log('myoutputcontexts: '+myoutputcontexts);
+    var contextSurgery = myoutputcontexts.parameters.surgery;
+    
+    console.log(contextSurgery);
+
     return agent.add('而根據其他病人分享的資料，做胃鏡且收費接近中位數的醫生有：1.王大文2.陳阿文3.房小文\n個別醫生收費有異、醫療服務收費會修改、而病人情況也因人而異，這些都是影響收費的因素，如對收費有疑問請向你的醫生請教。本平台只蒐集病人個案，會力求資料正確，內容只供參考之用。未得同意，不得作其他商業用途。惟最終資料準確性，請諮詢醫生及專業人士。\n\n如有其他的查詢和分享，請輸入1；若無，請輸入2(in sail)');
     
   } 
